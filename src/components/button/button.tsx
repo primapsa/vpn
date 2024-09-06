87a31da01ba862ab39c6c394b88f0c8a9c3ad207
Sergey Primakovich <prima.psa@gmail.com>
2024-09-06 21:22:19 +0300

    fix: button width

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -21,7 +21,7 @@ export default Button;
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     variant?: 'primary' | 'secondary';
-    width?: 'content' | 'full';
+    wd?: 'content' | 'full';
     className?: string;
     children: React.ReactNode
 }
