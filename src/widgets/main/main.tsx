619b00ea364ce2e4c67ef0cee411124a5d7cf224
Sergey Primakovich <prima.psa@gmail.com>
2024-09-06 21:25:14 +0300

    feat: add main component

---
+++ src/widgets/main/main.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './main.module.scss'
+import Image from "next/image";
+import {Button, Plate, Reward} from "@/components";
+import { plates } from '@/constants'
+import UserImg from './../../assets/images/vpn_user.png'
+const Main = ({onClick}:MainProps) => {
+    const plateList = plates.map((p,i) => <Plate key={i} {...p}/> )
+    return (
+        <section className={styles.section}>
+            <h3 className={styles.title}>Access <span className={styles.title_highlitghed}>everything
+                securely</span> with <br/>VPN</h3>
+            <Image  className={styles.img} src={UserImg as string} alt='vpn user' width={540} height={210}/>
+            <Button variant="primary" onClick={onClick}>Get VPN</Button>
+            <Reward/>
+            <div className={styles.plates}>
+                <div className={styles.plates__container}>
+                    {plateList}
+                </div>
+            </div>
+            <p className={styles.text}>VPN-yours <span className={styles.text_highlighted}>ultimate</span> solution for a private and secure online experience!</p>
+        </section>
+    );
+};
+
+export default React.memo(Main);
+
+interface MainProps {
+    onClick: () => void;
+    className?: string;
+}
