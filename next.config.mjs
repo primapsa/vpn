f4326719019b4269e82821cbbaf7db1dcd9f5d90
Sergey Primakovich <prima.psa@gmail.com>
2024-09-06 11:48:15 +0300

    init: start vpn landing task

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+
+const nextConfig = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+};
 
 export default nextConfig;
